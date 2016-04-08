be5b31d6d5fe1c129d7422503631bc4c172ca4c2
emersion <contact@emersion.fr>
2016-04-08 16:38:05 +0200

    Adds logs

--- src/Microbes.java
+++ src/Microbes.java
@@ -10,17 +10,22 @@ public class Microbes {
 	public static void main(String[] args) throws Exception {
 		LoadSave ls = new LoadSave();
 
+		System.out.println("Loading dataset...");
 		Instances input = ls.loadDataset(DATASET_PATH);
 		input.setClassIndex(input.numAttributes() - 1);
 
+		System.out.println("Filtering attributes...");
 		ArrayList<Instances> filtered = selection_variables.filtres(input);
 
+		System.out.println("Selecting best classifier...");
 		BestClassifierSelector bcs = new BestClassifierSelector();
 		ArrayList<Classifier> classifiers = bcs.select(filtered.get(0));
 
+		System.out.println("Initializing vote...");
 		DecisionMaker dm = new MajorityDecisionMaker();
 		Voter voter = new Voter(dm, classifiers);
 
+		System.out.println("Classifying instances...");
 		try {
 			Instance inst = null;
 			int result = voter.classifyInstance(inst);
@@ -28,5 +33,7 @@ public class Microbes {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
+		System.out.println("Done!");
 	}
 }
