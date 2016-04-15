5d2e8b5ed5226c007c53ba059294074feba27c80
FooB4r <bambambambooboo@gmail.com>
2016-04-15 17:06:53 +0200

    Fix bugs in CompareDecisionMaker 	add a little test (in main)

--- src/CompareDecisionMaker.java
+++ src/CompareDecisionMaker.java
@@ -11,13 +11,16 @@ public class CompareDecisionMaker {
 	
 	public CompareDecisionMaker(String path){
 		this.filePath = path;
+		this.dms = new ArrayList<DecisionMaker>();
 	}
 	
 	public CompareDecisionMaker(String path, ArrayList<DecisionMaker> dmList){
 		this.filePath = path;
+		this.dms = new ArrayList<DecisionMaker>();
 	}
 	
 	public void addDecisionMaker(DecisionMaker dm){
+		this.dms = new ArrayList<DecisionMaker>();
 		this.dms.add(dm);
 	}
 
@@ -25,11 +28,13 @@ public class CompareDecisionMaker {
 		HashMap<String, Integer> results = new HashMap<String, Integer>();
 		for(int i = 0; i < this.dms.size(); i++){
 			//Il va falloir gérer les doublons
-			String name = this.dms.getClass().toString();
+			String name = this.dms.get(i).getClass().toString();
+			name = name.split(" ")[1]; //Dégeu mais marche
 			results.put(name, this.dms.get(i).decide(vote));
 		}
 		return results;
 	}
+	
 
 	public void saveCompareDM(HashMap<String, Integer> results) {
 		PrintWriter writer;
@@ -44,5 +49,17 @@ public class CompareDecisionMaker {
 			writer.println(name + "," + results.get(name));
 		}
 		writer.close();
+	}
+	
+	public static void main(String args[]){
+		//A little test
+		CompareDecisionMaker comp = new CompareDecisionMaker("/home/raphael.cornet/Documents/MiniProj/compare.csv");
+		double[] vote = {0,1,2,3,4,5};
+		MajorityDecisionMaker majority = new MajorityDecisionMaker();
+		majority.decide(vote);
+		
+		comp.addDecisionMaker(majority);
+		
+		comp.saveCompareDM(comp.compare(vote));
 	}
 }
